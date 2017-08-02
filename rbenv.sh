b8be792e352fed1badd58fad0f7cbfac0f02ab25
Fernando Papito <fernando@qaninja.com.br>
2017-08-02 18:36:06 -0300

    fix bashscript

--- rbenv.sh
+++ rbenv.sh
@@ -4,3 +4,13 @@ echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 rbenv global 2.3.3
 gem install bundler
+
+bash -c "[ -d \$HOME/.rbenv ]"
+bash -c "[ -d \$HOME/.rbenv/plugins/ruby-build ]"
+bash -c "env RBENV_ROOT\=\$HOME/.rbenv RBENV_VERSION\=2.3.3 CONFIGURE_OPTS\= RUBY_CONFIGURE_OPTS\= \$HOME/.rbenv/bin/rbenv local 2>/dev/null || true"
+bash -c "mkdir \$HOME/.rbenv/.lock"
+bash -c "env RBENV_ROOT\=\$HOME/.rbenv RBENV_VERSION\=2.3.3 CONFIGURE_OPTS\= RUBY_CONFIGURE_OPTS\= \$HOME/.rbenv/bin/rbenv versions --bare"
+bash -c "env RBENV_ROOT\=\$HOME/.rbenv RBENV_VERSION\=2.3.3 CONFIGURE_OPTS\= RUBY_CONFIGURE_OPTS\= \$HOME/.rbenv/bin/rbenv rehash"
+bash -c "env RBENV_ROOT\=\$HOME/.rbenv RBENV_VERSION\=2.3.3 CONFIGURE_OPTS\= RUBY_CONFIGURE_OPTS\= \$HOME/.rbenv/bin/rbenv exec gem list"
+bash -c "env RBENV_ROOT\=\$HOME/.rbenv RBENV_VERSION\=2.3.3 CONFIGURE_OPTS\= RUBY_CONFIGURE_OPTS\= \$HOME/.rbenv/bin/rbenv rehash"
+bash -c "rm -rf \$HOME/.rbenv/.lock"
